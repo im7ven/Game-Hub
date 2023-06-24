16c16730493f0a409a7d6c9b8b7c7bfc3ca5fe7a
7ven <alexanderrr416@gmail.com>
2023-06-24 18:11:13 -0400

    Build platform menu.

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,11 @@
+import useData from "./useDate";
+
+interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+const usePlatform = () => useData<Platform>("/platforms/lists/parents");
+
+export default usePlatform;
